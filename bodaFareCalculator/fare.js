f10c4524f6dd21b5e452a4de7327ae8ba90c93e4
Brench-maina <bein67544@gmail.com>
2025-06-10 18:20:26 +0300

    Update fare.js

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,6 +1,6 @@
 function calculateBodaFare(distanceInKm){
-  const baseFare = 50;  // Base fare for the ride
- const chargePerKilometer = 15; // charge per kilometer
+  const baseFare = 50;         // Base fare for the ride
+ const chargePerKilometer = 15;    // charge per kilometer
  const tripFare = distanceInKm * chargePerKilometer; // calculate the fare based on distance
     const totalFare = baseFare + tripFare; // calculate the total fare
 
@@ -8,7 +8,7 @@ console.log(`Uko Kwote? Io ni ${distanceInKm} km:`);
 console.log(`Ukikalia Pikipiki: KES ${baseFare}`);
 console.log(`Mpaka Uko: KES ${tripFare}`);
 console.log(`Total: KES ${totalFare}`);
-console.log(`\nPanda Pikipiki`);
+console.log(`Panda Pikipiki`);
 
 }
 // prompt the user
